ffda36b2a822e031754691322cff103fb48aa35d
Brendan Heussler <brendan@lifejourney.us>
2015-03-01 08:54:39 -0500

    Move stuff around

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,6 @@ var multer = require('multer');
 var router = express.Router();
 var done = false;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/api/photo', function(req, res) {
-  if(done==true) {
-    console.log(req.files);
-    res.end('File uploaded.');
-  }
-});
-
 router.use(multer({ dest: './uploads',
   rename: function (fieldname, filename) {
     return filename+Date.now();
@@ -27,5 +15,17 @@ router.use(multer({ dest: './uploads',
     done=true;
   }
 }));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/api/photo', function(req, res) {
+  if(done==true) {
+    console.log(req.files);
+    res.end('File uploaded.');
+  }
+});
 
 module.exports = router;
